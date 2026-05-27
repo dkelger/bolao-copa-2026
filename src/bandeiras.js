ba30add6fd1292c5059363529aec1aa9c59c677c
Diego Elger <dkelger@gmail.com>
2026-05-27 16:22:54 -0400

    feat: arquivo bandeiras centralizado

---
+++ src/bandeiras.js
@@ -0,0 +1,16 @@
+const BANDEIRAS = {
+  'México': '🇲🇽', 'África do Sul': '🇿🇦', 'Coreia do Sul': '🇰🇷', 'República Tcheca': '🇨🇿',
+  'Canadá': '🇨🇦', 'Catar': '🇶🇦', 'Suíça': '🇨🇭', 'Itália': '🇮🇹',
+  'Brasil': '🇧🇷', 'Marrocos': '🇲🇦', 'Haiti': '🇭🇹', 'Escócia': '🏴󠁧󠁢󠁳󠁣󠁴󠁿',
+  'Estados Unidos': '🇺🇸', 'Paraguai': '🇵🇾', 'Austrália': '🇦🇺', 'Turquia': '🇹🇷',
+  'Alemanha': '🇩🇪', 'Curaçao': '🇨🇼', 'Costa do Marfim': '🇨🇮', 'Equador': '🇪🇨',
+  'Holanda': '🇳🇱', 'Japão': '🇯🇵', 'Tunísia': '🇹🇳', 'Ucrânia': '🇺🇦',
+  'Bélgica': '🇧🇪', 'Egito': '🇪🇬', 'Irã': '🇮🇷', 'Nova Zelândia': '🇳🇿',
+  'Espanha': '🇪🇸', 'Cabo Verde': '🇨🇻', 'Arábia Saudita': '🇸🇦', 'Uruguai': '🇺🇾',
+  'França': '🇫🇷', 'Senegal': '🇸🇳', 'Noruega': '🇳🇴', 'Iraque': '🇮🇶',
+  'Argentina': '🇦🇷', 'Argélia': '🇩🇿', 'Áustria': '🇦🇹', 'Jordânia': '🇯🇴',
+  'Portugal': '🇵🇹', 'Uzbequistão': '🇺🇿', 'Colômbia': '🇨🇴', 'RD Congo': '🇨🇩',
+  'Inglaterra': '🏴󠁧󠁢󠁥󠁮󠁧󠁿', 'Croácia': '🇭🇷', 'Gana': '🇬🇭', 'Panamá': '🇵🇦'
+}
+ 
+export default BANDEIRAS
